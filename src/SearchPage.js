7d6968b5f7667d79b7ab27078f840a48aeaa4c29
Albert Shala <albert.shala@gmail.com>
2018-03-05 14:55:46 -0800

    formatting update

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import sortBy from 'sort-by';
 import BookShelf from './Books/BookShelf';
@@ -6,86 +6,86 @@ import * as BooksAPI from './BooksAPI'
 
 class SearchPage extends Component {
 
-    constructor(props){
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            query: '',
-            books: []
-        };
-    }
+		this.state = {
+			query: '',
+			books: []
+		};
+	}
 
-  componentWillReceiveProps() {
-    	  console.log('query at', this.state.query);
-        this.updateQuery(this.state.query);
-  }
+	componentWillReceiveProps() {
+		this.updateQuery(this.state.query);
+	}
 
-  updateQuery = (query) => {
-      if ( query !== '' && query.length > 0 ) {
-        //set the query for the search bar
-        this.setState({query: query});
-        let result = [];
-        //Make a request to the search api, remove white spaces from query
-        BooksAPI.search(query.trim())
-        .then((searchResults) => {
-            result = searchResults && searchResults.length > 0
-                && searchResults.map(book => {
-                //default set category to none
-                book.shelf = 'none';
-                //loop over books from main route
-                this.props.books.forEach(passedInBooks => {
-                    if ( passedInBooks.id === book.id ){
-                        book.shelf = passedInBooks.shelf;
-                    }
-                });
+	updateQuery = (query) => {
+		if (query !== '' && query.length > 0) {
+			let result = [];
+			//Make a request to the search api, remove white spaces from query
+			BooksAPI.search(query.trim())
+				.then((searchResults) => {
+					result = searchResults && searchResults.length > 0
+						&& searchResults.map(book => {
+							//default set category to none
+							book.shelf = 'none';
+							//loop over books from main route
+							this.props.books.forEach(passedInBooks => {
+								if (passedInBooks.id === book.id) {
+									book.shelf = passedInBooks.shelf;
+								}
+							});
 
-                return book;
-            });
+							return book;
+						});
 
-            this.setState({
-                books: result
-            });
+					this.setState({
+						books: result,
+						query: query
+					});
 
-        }).catch((error) => {
-          console.error(error);
-        });
-      } else {
-          this.setState({query: '', books: []});
-      }
-  };
+				}).catch((error) => {
+				console.error(error);
+			});
+		} else {
+			this.setState({query: '', books: []});
+		}
+	};
 
-  render() {
-    const {query, books} = this.state;
-    const {currentCategory} = this.props;
-    let showBooks = [], updatedShelfs = [];
+	render() {
+		const {query, books} = this.state;
+		const {currentCategory} = this.props;
+		let showBooks = [], updatedShelfs = [];
 
-    if ( query ) {
-        if ( books && books.length > 0 ){
-            showBooks = books;
-	          sortBy(showBooks, 'title');
-            updatedShelfs = currentCategory.concat('none');
-        } else {
-            showBooks = [];
-        }
-    }
+		if (query) {
+			if (books && books.length > 0) {
+				showBooks = books;
+				sortBy(showBooks, 'title');
+				updatedShelfs = currentCategory.concat('none');
+			} else {
+				showBooks = [];
+			}
+		}
 
-    return(
-      <div className="search-books">
-      <div className="search-books-bar">
-        <Link to={{
-          pathname: '/',
-          state: {showSearchPage: false}
-        }} className='close-search'>Close</Link>
-        <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event)=> this.updateQuery(event.target.value)}/>
-        </div>
-      </div>
-      <div className="search-books-results">
-        <BookShelf books={showBooks} currentCategory={ updatedShelfs } onUpdateBookCategory={this.props.onUpdateBookCategory } />
-      </div>
-    </div>
-    )
-  }
+		return (
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link to={{
+						pathname: '/',
+						state: {showSearchPage: false}
+					}} className='close-search'>Close</Link>
+					<div className="search-books-input-wrapper">
+						<input type="text" placeholder="Search by title or author" value={this.state.query}
+						       onChange={(event) => this.updateQuery(event.target.value)}/>
+					</div>
+				</div>
+				<div className="search-books-results">
+					<BookShelf books={showBooks} currentCategory={updatedShelfs}
+					           onUpdateBookCategory={this.props.onUpdateBookCategory}/>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default SearchPage
